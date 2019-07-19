10511bfc392ba331de79fb5734790a98045fc041
Graham Fairweather <xotic750@gmail.com>
2019-07-20 00:23:48 +0200

    :bookmark: v4.0.5
    Auto build

--- dist/bind-x.js
+++ dist/bind-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-18T22:44:32.246Z",
+  "date": "2019-07-19T22:23:42.417Z",
   "describe": "",
   "description": "Creates a new function with a bound sequence of arguments.",
   "file": "bind-x.js",
   "hash": "3800df5f0f00c50fd3b3",
   "license": "MIT",
-  "version": "4.0.4"
+  "version": "4.0.5"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
